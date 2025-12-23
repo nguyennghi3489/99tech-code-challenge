d02c705be4f080b94159ec83de158c02462b3dc2
Nghi Nguyen <nguyennghi3489@gmail.com>
2025-12-23 22:40:40 +0700

    feat: remove stale time

--- src/problem2/src/hooks/use-get-token-list.ts
+++ src/problem2/src/hooks/use-get-token-list.ts
@@ -5,7 +5,6 @@ export const useGetTokenList = () => {
   return useQuery({
     queryKey: ["tokens"],
     queryFn: getTokenList,
-    staleTime: 1000 * 60 * 5, // 5 minutes
     refetchOnWindowFocus: false,
   });
 };
